fbb3607ded6890a13817cdeb899df4a525e16838
Waleedul Haque <57393298+waleedul3@users.noreply.github.com>
2025-11-23 23:52:33 +0530

    Update CORS to allow Vercel domains

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex='https://.*\.vercel\.app',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
